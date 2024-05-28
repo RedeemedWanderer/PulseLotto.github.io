db384835411229659b9ae4b0c0131857b5f95fcb
RedeemedWanderer <170952724+RedeemedWanderer@users.noreply.github.com>
2024-05-28 11:57:33 -0400

    app.js

--- app.js
+++ app.js
@@ -12,13 +12,18 @@ document.getElementById('connectButton').addEventListener('click', connectMetaMa
 async function connectMetaMask() {
     if (window.ethereum) {
         web3 = new Web3(window.ethereum);
-        await window.ethereum.enable();
-        const accounts = await web3.eth.getAccounts();
-        account = accounts[0];
-        document.getElementById('account').innerText = `Connected account: ${account}`;
-        contract = new web3.eth.Contract(contractABI, contractAddress);
-        checkEligibility();
-        getDrawStats();
+        try {
+            await window.ethereum.request({ method: 'eth_requestAccounts' });
+            const accounts = await web3.eth.getAccounts();
+            account = accounts[0];
+            document.getElementById('account').innerText = `Connected account: ${account}`;
+            contract = new web3.eth.Contract(contractABI, contractAddress);
+            checkEligibility();
+            getDrawStats();
+            getWinners();
+        } catch (error) {
+            console.error('User denied account access or error occurred');
+        }
     } else {
         alert('MetaMask not detected!');
     }
@@ -41,6 +46,16 @@ async function getDrawStats() {
     document.getElementById('lastAnnual').innerText = new Date(lastDistributions[2] * 1000).toLocaleString();
 }
 
+async function getWinners() {
+    const lastBiWeeklyWinner = await contract.methods.getLastBiWeeklyWinner().call();
+    const lastSemiQuarterlyWinner = await contract.methods.getLastSemiQuarterlyWinner().call();
+    const lastAnnualWinner = await contract.methods.getLastAnnualWinner().call();
+
+    document.getElementById('lastBiWeeklyWinner').innerText = lastBiWeeklyWinner;
+    document.getElementById('lastSemiQuarterlyWinner').innerText = lastSemiQuarterlyWinner;
+    document.getElementById('lastAnnualWinner').innerText = lastAnnualWinner;
+}
+
 window.addEventListener('load', () => {
     if (window.ethereum) {
         web3 = new Web3(window.ethereum);
@@ -48,3 +63,4 @@ window.addEventListener('load', () => {
         alert('MetaMask not detected!');
     }
 });
+
